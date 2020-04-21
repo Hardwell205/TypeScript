d8c4ee1b81dedac6b9dc3abed601c24841ed1367
Hardwell205 <48546565+Hardwell205@users.noreply.github.com>
2020-04-21 15:07:14 +0200

    fafa jak talala

--- kółko i krzyż/Tscript.js
+++ kółko i krzyż/Tscript.js
@@ -4,20 +4,69 @@ var Board = /** @class */ (function () {
         this.table = t;
         this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     }
+    Board.prototype.Draw = function () {
+        for (var i = 0; i < 9; i++) {
+            if (this.board[i] == 0)
+                return false;
+        }
+        return true;
+    };
     Board.prototype.ClickCell = function (x, y) {
-        var p = 3 * (x - 1) + (y - 1);
-        this.table[p].style.color = "#25bfc4";
-        this.table[p].innerHTML = "O";
-        this.table[p].style.color = "#25bfc4";
-        this.table[p].innerHTML = "X";
+        if (this.turn == true) {
+            var P = 3 * (x - 1) + (y - 1);
+            this.table[P].style.color = "#25bfc4";
+            this.table[P].innerHTML = "O";
+            this.board[P] = 1;
+            this.turn = false;
+            if (this.win(this.board) == 1) {
+                alert("Wygrywa O!");
+            }
+            if (this.Draw()) {
+                alert('Remis!');
+            }
+        }
+        else {
+            var P = 3 * (x - 1) + (y - 1);
+            this.table[P].style.color = "#25bfc4";
+            this.table[P].innerHTML = "X";
+            this.board[P] = -1;
+            this.turn = true;
+            if (this.win(this.board) == -1) {
+                alert("Wygrywa X!");
+            }
+            if (this.Draw()) {
+                alert('Remis!');
+            }
+        }
+    };
+    Board.prototype.win = function (board) {
+        var b = board[1];
+        if (board[0] == b && b == board[2] && b != 0)
+            return b;
+        b = board[4];
+        if (board[3] == b && b == board[5] && b != 0)
+            return b;
+        b = board[7];
+        if (board[6] == b && b == board[8] && b != 0)
+            return b;
+        b = board[3];
+        if (board[0] == b && b == board[6] && b != 0)
+            return b;
+        b = board[4];
+        if (board[1] == b && b == board[7] && b != 0)
+            return b;
+        b = board[5];
+        if (board[2] == b && b == board[8] && b != 0)
+            return b;
+        b = board[4];
+        if (board[0] == b && b == board[8] && b != 0)
+            return b;
+        if (board[2] == b && b == board[6] && b != 0)
+            return b;
+        return 0;
     };
     return Board;
 }());
-function next(x, y) {
-    var p = 3 * (x - 1) + (y - 1);
-    this.table[p].style.color = "#25bfc4";
-    this.table[p].innerHTML = "O";
-}
 window.onload = function () {
     var cell11 = document.getElementById("cell11");
     var cell12 = document.getElementById("cell12");
@@ -28,15 +77,14 @@ window.onload = function () {
     var cell31 = document.getElementById("cell31");
     var cell32 = document.getElementById("cell32");
     var cell33 = document.getElementById("cell33");
-    var button = document.getElementById("switch");
     var cell = new Board([cell11, cell12, cell13, cell21, cell22, cell23, cell31, cell32, cell33]);
-    cell11.onclick = function (e) { cell.ClickCell(1, 1); };
-    cell12.onclick = function (e) { cell.ClickCell(1, 2); };
-    cell13.onclick = function (e) { cell.ClickCell(1, 3); };
-    cell21.onclick = function (e) { cell.ClickCell(2, 1); };
-    cell22.onclick = function (e) { cell.ClickCell(2, 2); };
-    cell23.onclick = function (e) { cell.ClickCell(2, 3); };
-    cell31.onclick = function (e) { cell.ClickCell(3, 1); };
-    cell32.onclick = function (e) { cell.ClickCell(3, 2); };
-    cell33.onclick = function (e) { cell.ClickCell(3, 3); };
+    cell11.onclick = function () { cell.ClickCell(1, 1); };
+    cell12.onclick = function () { cell.ClickCell(1, 2); };
+    cell13.onclick = function () { cell.ClickCell(1, 3); };
+    cell21.onclick = function () { cell.ClickCell(2, 1); };
+    cell22.onclick = function () { cell.ClickCell(2, 2); };
+    cell23.onclick = function () { cell.ClickCell(2, 3); };
+    cell31.onclick = function () { cell.ClickCell(3, 1); };
+    cell32.onclick = function () { cell.ClickCell(3, 2); };
+    cell33.onclick = function () { cell.ClickCell(3, 3); };
 };
--- kółko i krzyż/Tscript.ts
+++ kółko i krzyż/Tscript.ts
@@ -1,27 +1,67 @@
 class Board {
     board: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     table: HTMLElement [];
-
+    turn: boolean;
     constructor(t:HTMLElement[]){
         this.table = t;         
         this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     }
-
-    ClickCell(x: number, y: number){
-        
-   
-        var p = 3 * (x - 1) + (y - 1);
-        this.table[p].style.color = "#25bfc4";
-        this.table[p].innerHTML = "O";
+    Draw(): boolean{
+        for (let i=0; i <9; i++){
+            if(this.board[i]==0)
+            return false;
         }
-    ClickCell1(x: number, y: number){
-        
-   
-            var p = 3 * (x - 1) + (y - 1);
-            this.table[p].style.color = "#25bfc4";
-            this.table[p].innerHTML = "O";
-            }
+        return true;
     }
+    ClickCell(x: number, y: number){
+        if(this.turn==true){
+
+        var P = 3 * (x - 1) + (y - 1);
+        this.table[P].style.color = "#25bfc4";
+        this.table[P].innerHTML = "O";
+        this.board[P]=1;
+        this.turn =false;
+        if(this.win(this.board)==1){
+                alert("Wygrywa O!");
+        }
+        if(this.Draw()){
+            alert('Remis!')
+        }
+            }
+        
+            else{
+
+            var P = 3 * (x - 1) + (y - 1);
+            this.table[P].style.color = "#25bfc4";
+            this.table[P].innerHTML = "X";
+            this.board[P]=-1;
+            this.turn=true;
+            if(this.win(this.board)==-1){
+                alert("Wygrywa X!");
+            }
+            if(this.Draw()){
+                alert('Remis!')
+            }
+                }
+        }
+        win(board: number[]): number {
+            var b = board[1];
+            if (board[0] == b && b == board[2] && b != 0) return b;
+            b = board[4];
+            if (board[3] == b && b == board[5] && b != 0) return b;
+            b = board[7];
+            if (board[6] == b && b == board[8] && b != 0) return b;
+            b = board[3];
+            if (board[0] == b && b == board[6] && b != 0) return b;
+            b = board[4];
+            if (board[1] == b && b == board[7] && b != 0) return b;
+            b = board[5];
+            if (board[2] == b && b == board[8] && b != 0) return b;
+            b = board[4];
+            if (board[0] == b && b == board[8] && b != 0) return b;
+            if (board[2] == b && b == board[6] && b != 0) return b;
+            return 0;
+}}
 
 window.onload = () => {
     let cell11: HTMLElement = <HTMLElement>document.getElementById("cell11");
@@ -35,13 +75,13 @@ window.onload = () => {
     let cell33: HTMLElement = <HTMLElement>document.getElementById("cell33");
 
 let cell: Board = new Board([cell11, cell12, cell13, cell21, cell22, cell23, cell31, cell32, cell33]);
-cell11.onclick = (e) => { cell.ClickCell(1, 1);}
-cell12.onclick = (e) => { cell.ClickCell(1, 2);}
-cell13.onclick = (e) => { cell.ClickCell(1, 3);}
-cell21.onclick = (e) => { cell.ClickCell(2, 1);}
-cell22.onclick = (e) => { cell.ClickCell(2, 2);}
-cell23.onclick = (e) => { cell.ClickCell(2, 3);}
-cell31.onclick = (e) => { cell.ClickCell(3, 1);}
-cell32.onclick = (e) => { cell.ClickCell(3, 2);}
-cell33.onclick = (e) => { cell.ClickCell(3, 3);}
+cell11.onclick = () => { cell.ClickCell(1, 1);}
+cell12.onclick = () => { cell.ClickCell(1, 2);}
+cell13.onclick = () => { cell.ClickCell(1, 3);}
+cell21.onclick = () => { cell.ClickCell(2, 1);}
+cell22.onclick = () => { cell.ClickCell(2, 2);}
+cell23.onclick = () => { cell.ClickCell(2, 3);}
+cell31.onclick = () => { cell.ClickCell(3, 1);}
+cell32.onclick = () => { cell.ClickCell(3, 2);}
+cell33.onclick = () => { cell.ClickCell(3, 3);}
 }
