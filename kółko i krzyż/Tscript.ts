eeb3e05bd43a01e5e68e072846c883a708e769b9
Hardwell205 <48546565+Hardwell205@users.noreply.github.com>
2020-04-22 13:37:01 +0200

    Update Tscript.ts

--- kółko i krzyż/Tscript.ts
+++ kółko i krzyż/Tscript.ts
@@ -16,12 +16,13 @@ class Board {
     ClickCell(x: number, y: number){
         if(this.turn==true){
             
-        var P = 3 * (x - 1) + (y - 1);
-        this.table[P].style.color = "#25bfc4";
-        this.table[P].innerHTML = "O";
-        this.board[P]=1;
-        this.turn =false;
-    
+                var P = 3 * (x - 1) + (y - 1);
+                this.table[P].style.color = "#25bfc4";
+                this.table[P].innerHTML = "O";
+                this.board[P]=1;
+                this.turn =false;
+            
+        
         if(this.win(this.board)==1){
                 alert("Wygrywa O!");
         }
